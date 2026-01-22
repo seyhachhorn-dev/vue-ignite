81f661ed99faabb05c022fce2a9c987bcf5ebda7
CHHORN Seyha <senjihachiro@gmail.com>
2026-01-22 15:07:17 +0700

    tip calculator

--- src/components/ReactiveFunction.vue
+++ src/components/ReactiveFunction.vue
@@ -65,18 +65,38 @@ const resetBill = () =>{
 import { reactive, ref, computed } from "vue";
 // state
 const billAmount = ref(0);
-const tipPercentages = reactive({
+const tipState = reactive({
   percentage: 0,
 });
+const historyLogs = ref([]);
 
 // updated when anything changes
 const tipCalculation = computed(() => {
-  return (billAmount.value * tipPercentages.percentage) / 100;
+  return (billAmount.value * tipState.percentage) / 100;
 });
+
+// grand total
+
+const gradeTotal = computed(() =>{
+    return billAmount.value + tipCalculation.value;
+})
+// reset
+
+const resetBill = () =>{
+    billAmount.value = 0;
+    tipState.percentage = 0;
+}
+
+const saveHistory = computed(() =>{
+    historyLogs.value.push(gradeTotal.value);
+})
+console.log({historyLogs});
+
 </script>
 
 <template>
-  <div>
+<main>
+      <div>
     <h1>Tip Calculator</h1>
     <label>Bill Amount: </label>
     <input
@@ -85,12 +105,34 @@ const tipCalculation = computed(() => {
       placeholder="Bill Amount"
     />
     <label>Tip percentage: </label>
-    <select v-model.number="tipPercentages.percentage">
+    <select v-model.number="tipState.percentage">
       <option value="10">10%</option>
       <option  value="20">20%</option>
       <option  value="30">30%</option>
     </select>
 
     <h2>TotalTips: {{ tipCalculation }}</h2>
+    <h2>gradeTotal : {{gradeTotal}}</h2>
+    <button @click="resetBill">Reset Bill</button>
+    <!-- save the history -->
+     <button @click="saveHistory"> Save History</button>
   </div>
+  <!-- history logs -->
+   <h1>History Logs</h1>
+   <ul v-if="historyLogs.length > 0">
+    <li v-for="(item,index) in historyLogs" :key="index">
+        Save Total Tip: {{item}} 
+    </li>    
+   </ul>
+       <p v-else>no history are save here!!</p>
+
+
+  <div>
+
+
+
+
+
+  </div>
+</main>
 </template>
